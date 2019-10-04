c8667f21c3fe815d6204b5be664e256e6fbb4df4
Corey Ling <kasuko@ionshard.com>
2019-10-04 19:26:44 -0400

    Update anvil-menu.js to allow future versions.

--- anvil-menu/anvil-menu.js
+++ anvil-menu/anvil-menu.js
@@ -1,27 +1,37 @@
 (() => {
-  window.AnvilMenu = window.AnvilMenu || {loaded: false, entries: []};
+  const version = 1;  //Current Anvil Menu Version
 
-  if(window.AnvilMenu.loaded) {
+  //Anvil Menu Bootstrap
+  if (!window.AnvilMenu) {
+    window.AnvilMenu = {loaded: 0};
+    window.AnvilMenu.setup = () => console.error('Anvil Menu | Failed to setup Anvil Menu');
+    $(() => window.AnvilMenu.setup());
+  }
+
+  if (window.AnvilMenu.loaded >= version) {
     return;
   }
-  window.AnvilMenu.loaded = true;
+  window.AnvilMenu.loaded = version;
 
-  console.log('Anvil Menu | Initializing');
+  window.AnvilMenu.setup = () => {
+    console.log(`Anvil Menu | Initializing v${version}`);
 
-  window.AnvilMenu.registerMenuEntry = entry => {
-    window.AnvilMenu.entries.push(entry);
+    window.AnvilMenu.entries = [];
+
+    window.AnvilMenu.registerMenuEntry = entry => {
+      window.AnvilMenu.entries.push(entry);
+    };
+
+    window.AnvilMenu.registerMenuEntries = entries => entries.forEach(window.AnvilMenu.registerMenuEntry);
+
+    Hooks.on('init', () => {
+      $('<div id="anvil-menu">')
+        .css('position', 'absolute')
+        .css('top', 12).css('left', 10)
+        .css('width', 100).css('height', 50)
+        .css('z-index', 61)
+        .appendTo($('body'));
+      window.AnvilMenu.contextMenu = new ContextMenu($('body'), '#anvil-menu', window.AnvilMenu.entries);
+    });
   };
-
-  window.AnvilMenu.registerMenuEntries = entries => entries.forEach(window.AnvilMenu.registerMenuEntry);
-
-  Hooks.on('init', () => {
-    $('<div id="anvil-menu">')
-      .css('position', 'absolute')
-      .css('top', 12).css('left', 10)
-      .css('width', 100).css('height', 50)
-      .css('z-index', 61)
-      .appendTo($('body'));
-    window.AnvilMenu.contextMenu = new ContextMenu($('body'), '#anvil-menu', window.AnvilMenu.entries);
-  });
 })();
-
